115af4a9befa3a0e0c73665bf40e6bd383b89bfc
oleggorina <oleg.gorina@gmail.com>
2024-08-03 12:57:56 +0300

    Complete auth res

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as CookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.use(CookieParser());
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
   await app.listen(3000);
 }
 bootstrap();
